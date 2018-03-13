393a8fc62f411af3198973d5cda9ad12a48e2edd
Richard Long <richard.long@nowhere>
2018-03-13 09:29:16 +0000

    debuging clipboard copy issue; added template vlc proxy service

---
+++ src/module.vlc/VlcProxyService.h
@@ -0,0 +1,12 @@
+//
+//  VlcProxyService.h
+//  McRemote
+//
+//  Created by lrlong on 11/03/2018.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VlcProxyService : NSObject
+
+@end
---
+++ src/module.vlc/VlcProxyService.m
@@ -0,0 +1,12 @@
+//
+//  VlcProxyService.m
+//  McRemote
+//
+//  Created by lrlong on 11/03/2018.
+//
+
+#import "VlcProxyService.h"
+
+@implementation VlcProxyService
+
+@end
